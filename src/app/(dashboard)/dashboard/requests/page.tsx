1d52e451b52bf592a160d49ac5887e1c0a29cce8
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-13 18:06:16 -0300

    feat: create and setup DashbaordRequestPage screen.

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,38 @@
+import { fetchRedis } from "@/helpers/redis"
+import { authOptions } from "@/lib/next-auth"
+import { getServerSession } from "next-auth"
+import { notFound } from "next/navigation"
+
+export default async function DashboardRequestsPage() {
+  const session = await getServerSession(authOptions)
+  if (!session) notFound()
+
+  const incomingSenderIds = await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`) as string[]
+
+  const incommingFriendsRequests = await Promise.all([
+    incomingSenderIds.map(async (senderId) => {
+      // eslint-disable-next-line no-undef
+      const sender = await fetchRedis('get', `user:${senderId}`) as User
+      return {
+        senderId,
+        senderEmail: sender.email
+      }
+    })
+  ])
+  return (
+    <main
+      className="pt-8"
+    >
+      <h1
+        className="font-bold text-5xl mb-8"
+      >
+        Add a friend
+      </h1>
+      <div
+        className="flex flex-col gap-4"
+      >
+        {/* <FriendRequests /> */}
+      </div>
+    </main>
+  )
+}
