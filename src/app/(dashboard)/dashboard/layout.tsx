79071842b9d9996fa4299add04ea18f0e9fb1510
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-13 17:53:30 -0300

    feat: Create FriendRequsetSidebarOptions component.

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,7 @@
+import { FriendRequestSidebarOptions } from '@/components/FriendRequestSidebarOptions'
 import { Icons, TIcon } from '@/components/Icons'
 import { SignOutButton } from '@/components/SignOutButton'
+import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/next-auth'
 import { getServerSession } from 'next-auth'
 import Image from 'next/image'
@@ -34,6 +36,9 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   if (!session) {
     notFound()
   }
+
+  // eslint-disable-next-line no-undef
+  const unseenRequestCount = (await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`) as User[]).length
 
   return (
     <div className='w-full flex h-screen'>
@@ -80,6 +85,14 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
                 })}
               </ul>
             </li>
+
+            <li>
+              <FriendRequestSidebarOptions
+                initialUnseenRequestCount={unseenRequestCount}
+                sessionId={session.user.id}
+              />
+            </li>
+
             <li className='-mx-6 mt-auto flex items-center'>
               <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
                 <div className="relative h-8 w-8 bg-gray-50">
