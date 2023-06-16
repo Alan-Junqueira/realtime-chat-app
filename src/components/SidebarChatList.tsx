82186988e07421d61090c84c846db292f5446de3
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-06-15 22:32:48 -0300

    style, feat: finalize SidebarChatList component style

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -40,8 +40,17 @@ export const SidebarChatList = ({ friends, sessionId }: ISidebarChatList) => {
             <a href={`/dashboard/chat/${chatHrefConstructor(
               sessionId,
               friend.id
-            )}`}>
-              Hello
+            )}`}
+              className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+            >
+              {friend.name}
+              {unseenMessagesCount > 0 && (
+                <div
+                  className="bg-indigo-600 font-medium text-xm text-white w-4 h-4 rounded-full flex justify-center items-center"
+                >
+                  unseenMessagesCount
+                </div>
+              )}
             </a>
           </li>)
       })}
